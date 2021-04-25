0eebdadd702a6f1eb59e5bdf8b92c675e5ec2e59
mayyadah <miss.mayadah1@gmail.com>
2021-04-25 13:20:49 +0300

    write html css and js

---
+++ app.js
@@ -0,0 +1,10 @@
+let userName = prompt('What is Your name?');
+console.log(userName);
+let userAge = prompt('can you please write your age?');
+console.log(userAge);
+let userAddress = prompt('Where do you live?');
+console.log(userAddress); 
+let favBook = prompt('what is your fav book?');
+console.log(favBook);
+
+alert('Welcome, Your name is ' + userName + ' and Your age is, ' + userAge + ' you live in ' + userAddress + ' and your fav Book is ' + favBook);
